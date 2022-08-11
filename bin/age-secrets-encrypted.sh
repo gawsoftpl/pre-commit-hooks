faf2606d09fe7d4fe20911ed95eb45669807c09a
Pitos <biuro@gawsoft.pl>
2022-08-11 03:43:15 +0200

    Test

--- bin/age-secrets-encrypted.sh
+++ bin/age-secrets-encrypted.sh
@@ -1,6 +1,27 @@
 #!/usr/bin/env bash
 
-echo $1
-echo $@
+dir=$1
 
-exit 1
+if [ ! -d $dir ];
+then
+    echo "$dir not exists"
+    exit 1
+fi
+
+was_no_encrypted=0
+for file in $(find $dir -type f );
+do
+    was_encrypted=`cat ${file} | grep 'BEGIN AGE ENCRYPTED FILE'`
+    if [ ${#was_encrypted} -eq 0 ];
+    then
+        echo "File ${file} is not encrypted"
+        was_no_encrypted=1
+    fi
+done
+
+if [ $was_no_encrypted -gt 0 ];
+then
+    exit 1
+fi
+
+exit 0
