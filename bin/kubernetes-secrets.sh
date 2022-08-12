3de0711714e54968b9b084faa93255fb0f3e59f7
Pitos <biuro@gawsoft.pl>
2022-08-12 22:16:42 +0200

    Add kubernetes secrets

--- bin/kubernetes-secrets.sh
+++ bin/kubernetes-secrets.sh
@@ -9,7 +9,7 @@ then
 fi
 
 was_no_encrypted=0
-for file in $@;
+for file in "${@}";
 do
     was_encrypted=`cat ${file} | grep 'kind: Secret'`
     was_encrypted2=`cat ${file} | grep 'kind: secret'`
