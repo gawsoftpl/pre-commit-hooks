bfb27946e8552fde17472ff88b065a67ac117764
Pitos <biuro@gawsoft.pl>
2022-08-12 22:10:42 +0200

    Add kubernetes secrets

---
+++ bin/kubernetes-secrets.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+dir=$1
+
+if [ ! -d $dir ];
+then
+    echo "$dir not exists"
+    exit 1
+fi
+
+was_no_encrypted=0
+for file in $(find $dir -type f );
+do
+    was_encrypted=`cat ${file} | grep 'kind: Secret'`
+    was_encrypted2=`cat ${file} | grep 'kind: secret'`
+
+    if [ ${#was_encrypted} -eq 0 ] || [ ${#was_encrypted2} -eq 0 ] ;
+    then
+        echo "File ${file} is not encrypted"
+        was_no_encrypted=1
+    fi
+done
+
+if [ $was_no_encrypted -gt 0 ];
+then
+    exit 1
+fi
+
+exit 0
