992980e3affb4ea0cac850fe6d3fd4d8dc1d84f8
Pitos <biuro@gawsoft.pl>
2022-08-13 03:58:21 +0200

    Add generated password

---
+++ bin/age-secret-encrypted.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+
+was_no_encrypted=0
+file=$1
+
+was_encrypted=`cat ${file} | grep 'BEGIN AGE ENCRYPTED FILE'`
+if [ ${#was_encrypted} -eq 0 ];
+then
+    echo "File ${file} is not encrypted"
+    was_no_encrypted=1
+fi
+
+if [ $was_no_encrypted -gt 0 ];
+then
+    exit 1
+fi
+
+exit 0
