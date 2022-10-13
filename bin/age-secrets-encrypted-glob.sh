de5fec900561933fd79621a9f751820dbcd10c38
Pitos <biuro@gawsoft.pl>
2022-10-14 00:04:38 +0200

    Fix first version

--- bin/age-secrets-encrypted-glob.sh
+++ bin/age-secrets-encrypted-glob.sh
@@ -9,7 +9,7 @@ then
 fi
 
 was_no_encrypted=0
-for file in $(find . -iname $dir -type f );
+for file in $(find . -iname "$dir" -type f );
 do
     was_encrypted=`cat ${file} | grep 'BEGIN AGE ENCRYPTED FILE'`
     if [ ${#was_encrypted} -eq 0 ];
